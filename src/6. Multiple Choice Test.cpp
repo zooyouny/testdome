720178fd3be9886b86671925364af338d409744c
Jooyeon Hwang <zooyouny@gmail.com>
2020-11-27 10:27:14 +0900

    Update 6. Multiple Choice Test.cpp

--- src/6. Multiple Choice Test.cpp
+++ src/6. Multiple Choice Test.cpp
@@ -1,3 +1,11 @@
+/*
+    Multiple choice test has several multiple choice questions. Each question can have only one correct answer. 
+    Additionally, timed multiple choice test can specify the time allowed for solving each question in the test.
+    
+    The code below satisfies this specification, but the customer complained that the memory usage of the program 
+    constantly increases. Fix this problem.
+*/
+
 #include <iostream>
 #include <string>
 
